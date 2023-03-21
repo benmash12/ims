23c8df4893bfcdb565e812f8cd0817b8213c66e9
benmash12 <gbiangb@gmail.com>
2023-03-22 04:38:22 +0530

    ep

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace ims
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CloseBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LoginUsername = new System.Windows.Forms.TextBox();
+            this.LoginPass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.Loading = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Loading)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CloseBtn
+            // 
+            this.CloseBtn.BackColor = System.Drawing.Color.Transparent;
+            this.CloseBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.CloseBtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.CloseBtn.FlatAppearance.BorderSize = 0;
+            this.CloseBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.CloseBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.CloseBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseBtn.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CloseBtn.ForeColor = System.Drawing.Color.Firebrick;
+            this.CloseBtn.Location = new System.Drawing.Point(325, 6);
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.Size = new System.Drawing.Size(44, 44);
+            this.CloseBtn.TabIndex = 1;
+            this.CloseBtn.Text = "x";
+            this.CloseBtn.UseVisualStyleBackColor = false;
+            this.CloseBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(0, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(369, 41);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ADMIN LOGIN";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LoginUsername
+            // 
+            this.LoginUsername.BackColor = System.Drawing.Color.White;
+            this.LoginUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LoginUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginUsername.ForeColor = System.Drawing.Color.Black;
+            this.LoginUsername.Location = new System.Drawing.Point(40, 154);
+            this.LoginUsername.Name = "LoginUsername";
+            this.LoginUsername.Size = new System.Drawing.Size(288, 29);
+            this.LoginUsername.TabIndex = 3;
+            // 
+            // LoginPass
+            // 
+            this.LoginPass.BackColor = System.Drawing.Color.White;
+            this.LoginPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LoginPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginPass.ForeColor = System.Drawing.Color.Black;
+            this.LoginPass.Location = new System.Drawing.Point(40, 234);
+            this.LoginPass.Name = "LoginPass";
+            this.LoginPass.PasswordChar = '*';
+            this.LoginPass.Size = new System.Drawing.Size(288, 29);
+            this.LoginPass.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(37, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(37, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password";
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.LoginBtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.LoginBtn.FlatAppearance.BorderSize = 0;
+            this.LoginBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.LoginBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.LoginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LoginBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginBtn.ForeColor = System.Drawing.Color.White;
+            this.LoginBtn.Location = new System.Drawing.Point(134, 303);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(106, 35);
+            this.LoginBtn.TabIndex = 7;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = false;
+            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
+            // 
+            // Loading
+            // 
+            this.Loading.BackColor = System.Drawing.Color.Transparent;
+            this.Loading.Enabled = false;
+            this.Loading.Image = ((System.Drawing.Image)(resources.GetObject("Loading.Image")));
+            this.Loading.Location = new System.Drawing.Point(249, 305);
+            this.Loading.Name = "Loading";
+            this.Loading.Size = new System.Drawing.Size(32, 32);
+            this.Loading.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Loading.TabIndex = 8;
+            this.Loading.TabStop = false;
+            this.Loading.Visible = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(372, 380);
+            this.Controls.Add(this.Loading);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LoginPass);
+            this.Controls.Add(this.LoginUsername);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CloseBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Loading)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button CloseBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox LoginUsername;
+        private System.Windows.Forms.TextBox LoginPass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button LoginBtn;
+        private System.Windows.Forms.PictureBox Loading;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace ims
+{
+    public partial class LoginForm : Form
+    {
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int CS_DROPSHADOW = 0x20000;
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle |= CS_DROPSHADOW;
+                cp.Style |= CS_DROPSHADOW;
+                return cp;
+            }
+        }
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            var cr = MessageBox.Show("Ar you sure you want to exit?","Confirm Exit", MessageBoxButtons.YesNo);
+            if(cr == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+        }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            string un = LoginUsername.Text;
+            string ps = LoginPass.Text;
+            if(string.IsNullOrEmpty(un) || string.IsNullOrEmpty(ps))
+            {
+                MessageBox.Show("All fields are required!", "ERROR", MessageBoxButtons.OK);
+            }
+            else
+            {
+                LoginBtn.Enabled = false;
+                Loading.Visible = true;
+                DB d = new DB();
+                if(d.createTables() == 1){
+                    LoginBtn.Enabled = false;
+                    Loading.Visible = false;
+                    MessageBox.Show("App Initialised!", "SUCCESS", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    LoginBtn.Enabled = false;
+                    Loading.Visible = false;
+                    MessageBox.Show("App tables could not be created!", "ERROR", MessageBoxButtons.OK);
+                }
+            }
+        }
+    }
+    class DB
+    {
+        SqlConnection createConn()
+        {
+            string constr = "Data Source=(localdb)\\ProjectModels;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection conn = new SqlConnection(constr);
+            return conn;
+        }
+        public int createTables()
+        {
+            try
+            {
+                SqlConnection conn = createConn();
+                string query = @"IF OBJECT_ID(N'ins_admin', N'U') IS NULL CREATE TABLE ims_admin (id int identity(1,1) primary key, username varchar(20) NOT NULL, password text);";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                /*cmd.CommandText = 
+                cmd.ExecuteNonQuery();*/
+                cmd.CommandText = @"IF OBJECT_ID(N'ins_categories', N'U') IS NULL CREATE TABLE ims_categories (id int identity(1,1) primary key, category varchar(50) NOT NULL);";
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = @"IF OBJECT_ID(N'ins_products', N'U') IS NULL CREATE TABLE ims_products (id int identity(1,1) primary key, name varchar(100) NOT NULL, category_id int NOT NULL, quantity int DEFAULT '0', last_modified varchar(40) NOT NULL);";
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = @"IF OBJECT_ID(N'ins_inc_exp', N'U') IS NULL CREATE TABLE ims_inc_exp (id int identity(1,1) primary key, type varchar(10) NOT NULL, title varchar(100), val float(10,2) DEFAULT '0.00', date_added DATE default CURRENT_DATE); ";
+                cmd.ExecuteNonQuery();
+                /*cmd.CommandText = @"IF EXISTS (SELECT * FROM ims_admin WHERE username='admin') ELSE INSERT INTO ims_admin (username,password) VALUES('admin','pass');";
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = @"SET sql_notes = 1;";
+                cmd.ExecuteNonQuery();*/
+                conn.Close();
+                return 1;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                return 0;
+            }
+        }
+        public bool initApp()
+        {
+            try
+            {
+
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
+
